d7677f6c62ff7e6d8a94e94c15845db697c914d4
D shivnath <dawkeshivnath@gmail.com>
2026-01-06 17:21:30 +0530

    public s3

---
+++ 05-storage/Public-S3-Bucket/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "nautilus-s3-5406"
+}
+
+resource "aws_s3_bucket_acl" "public_acl" {
+  bucket = aws_s3_bucket.public_bucket.id
+  acl    = "public-read"
+}
+
